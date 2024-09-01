28ea32e7f4707a21123cf1310a84c2dd5f08688e
Mukul Kumar Sharma <mukulkumarsharma8@gmail.com>
2024-09-01 09:23:36 +0530

    pratical if else using ternary op and also use git stash

---
+++ nested_loop.py
@@ -0,0 +1,16 @@
+'''year = 2000
+if (year % 4)==0:
+    if (year % 100)==0:
+        if(year % 400)==0:
+            print("{0} is a leap year".format(year))
+        else:
+            print("{0} is not a leap year".format(year))'''
+
+name = "mukul"
+mid_name = "kumar"
+last_name = "sharma"
+branch ="cs"
+final_format = "Name :{},Mid_Name :{},Lat_Nmae :{},Branch :{}".format(name,mid_name,last_name,branch)
+print(final_format)
+
+
