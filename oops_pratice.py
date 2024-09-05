58f4890e63711378417f70e7947256d1b7c1f490
Mukul Kumar Sharma <mukulkumarsharma8@gmail.com>
2024-09-05 10:33:39 +0530

    oops_pratice

---
+++ oops_pratice.py
@@ -0,0 +1,29 @@
+class Car:
+    #constructor Called
+     def __init__(self, brand, model, color):
+         self.brand = brand
+         self.model = model
+         self.color = color
+     def car_details(self):
+         return f"Brand :{self.brand}, Model :{self.model}, color:{self.color}"
+     def start(self):
+         return f"{self.brand} {self.model} is starting."
+my_car=Car("toyota","corola","red")
+print(my_car.car_details())
+print(my_car.start())
+
+
+class Electriccar(Car):
+    def __init__(self,brand,model,color,battery_capacity):cd
+        super().__init__(brand,model,color)
+        self.battery_capacity = battery_capacity
+    def car_details(self):
+        original_details = super().car_details()
+        return f"{original_details},Battery Capacity: {self.battery_capacity} kwh"
+    def charge(self):
+        return f"{self.brand} {self.model} is charging."
+my_electric_car = Electriccar("Tesla","Model s","white",100)
+print(my_electric_car.car_details())
+print(my_electric_car.start())
+print(my_electric_car.charge())
+
